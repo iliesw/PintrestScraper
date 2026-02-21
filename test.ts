56b0076db4438d84b7be2949a9187e3989ef2bd1
iliesw <iliesmraihia@gmail.com>
2026-02-21 19:10:37 +0100

    scraper lite

--- test.ts
+++ test.ts
@@ -1,16 +1,24 @@
-import { PinterestScraper } from "./scrape";
+// import { PinterestScraper } from "./scrape";
 
-const scraper = new PinterestScraper({
-  headless: true,
-  scrollCount: 0,   // more scrolls = more pins
-  delay: 0,
-});
+import retrievePins from "./scrapelite";
 
-await scraper.init();
+// const scraper = new PinterestScraper({
+//   headless: true,
+//   scrollCount: 0,   // more scrolls = more pins
+//   delay: 0,
+// });
+// console.time("init")
+// await scraper.init();
+// console.timeEnd("init")
+
+// console.time("search")
+// const pins = await scraper.searchPins("ads illustration poster");
+// console.timeEnd("search")
+
+// console.log(pins);
+// await scraper.close();
+
 
 console.time("search")
-const pins = await scraper.searchPins("ads illustration poster");
+const pins = await retrievePins("ads illustration poster")
 console.timeEnd("search")
-
-console.log(pins);
-await scraper.close();
